690dc7872dfb2a1a66607a81c36a79b1abbf33d4
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-09 23:50:24 +0500

    Doing some changes in the style.js

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,7 @@
 const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
   
-    heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
+    heading2: "font-poppins font-semibold xs:text-[6vw] text-[40px] text-white",
     paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
   
     flexCenter: "flex justify-center items-center",
@@ -16,7 +16,7 @@ const styles = {
   };
   
   export const layout = {
-    section: `flex md:flex-row flex-col ${styles.paddingY}`,
+    section: `flex md:flex-row flex-col  ${styles.paddingY}`,
     sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
   
     sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
