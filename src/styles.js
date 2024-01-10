da28798dbf9e5ff10c4d779493226ddd28d9047c
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-10 20:03:48 +0500

    adding another para style in styles

--- src/styles.js
+++ src/styles.js
@@ -1,28 +1,30 @@
 const styles = {
-    boxWidth: "xl:max-w-[1280px] w-full",
-  
-    heading2: "font-poppins font-semibold xs:text-[6vw] text-[40px] text-white",
-    paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
-  
-    flexCenter: "flex justify-center items-center",
-    flexStart: "flex justify-center items-start",
-  
-    paddingX: "sm:px-16 px-6",
-    paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 px-6 sm:py-12 py-4",
-  
-    marginX: "sm:mx-16 mx-6",
-    marginY: "sm:my-16 my-6",
-  };
-  
-  export const layout = {
-    section: `flex md:flex-row flex-col  ${styles.paddingY}`,
-    sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
-  
-    sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
-    sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
-  
-    sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
-  };
-  
-  export default styles;
+  boxWidth: "xl:max-w-[1280px] w-full",
+
+  heading2: "font-poppins font-semibold xs:text-[6vw] text-[40px] text-white",
+  heading3: "text-[3.8vw] max-ss:text-[6vw] max-md:text-[5vw] font-poppins text-white font-semibold",
+  paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
+  paragraph2: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-ss:text-[14px] max-sm:text-[15px] max-sm:w-full",
+
+  flexCenter: "flex justify-center items-center",
+  flexStart: "flex justify-center items-start",
+
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-12 py-4",
+
+  marginX: "sm:mx-16 mx-6",
+  marginY: "sm:my-16 my-6",
+};
+
+export const layout = {
+  section: `flex md:flex-row flex-col  ${styles.paddingY}`,
+  sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+
+  sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
+  sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
+
+  sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
+};
+
+export default styles;
